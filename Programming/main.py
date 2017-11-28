09270bd044442ea35362ef40c69c435bce7d0ecf
DorgivalSilva <33164186+DorgivalSilva@users.noreply.github.com>
2017-11-28 11:32:52 -0300

    Add files via upload

--- Programming/main.py
+++ Programming/main.py
@@ -3,77 +3,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class MultiLayerPerceptron():
+class MultiLayerPerceptron:
 	def __init__(self, features, targets):
 		self.features = features
 		self.targets  = targets
 
 		self.nhidden = int((self.features.shape[1] + self.targets.shape[1])/2) + 1
+		
+		self.w12 = np.random.uniform(size = (self.features.shape[1], self.nhidden))
+		self.w23 = np.random.uniform(size  =(self.nhidden, self.targets.shape[1]))
 
-		self.w12 = 2*np.random.rand(self.features.shape[1], self.nhidden) - 1
-		self.w23 = 2*np.random.rand(self.nhidden, self.nhidden)  		  - 1
-		self.w34 = 2*np.random.rand(self.nhidden, self.targets.shape[1])  - 1
+		funcs = {'sigmoid': (lambda x: 1/(1 + np.exp(-x)), lambda x: x*(1 - x)),
+				 'linear' : (lambda x: x, lambda x: 1)}
+		(self.activate,  self.activatePrime)  = funcs['sigmoid']
+		(self.zactivate, self.zactivatePrime) = funcs['sigmoid']
 
-		activation = 'sigmoid'
-		funcs = {'sigmoid': (lambda x: 1/(1 + np.exp(-x)), lambda x: (1/(1+np.exp(-x))) * (1 - 1/(1 +np.exp(-x)))),
-				 'linear':  (lambda x: x, lambda x: 1)}
-		(self.activate, self.activatePrime) = funcs[activation]
 
 	def run(self, features):
-		h0 = features
-		h1 = self.activate(np.dot(h0, self.w12))
-		h2 = self.activate(np.dot(h1, self.w23))
-		z  = self.activate(np.dot(h2, self.w34))
-		return z		
+		v0 = features
+		y1 = self.activate(np.dot(v0, self.w12))
+		y2  = self.activate(np.dot(y1, self.w23))
+		return y2		
+
+	def shuffle_data(self, features, targets):
+		idx = np.random.permutation(len(features))
+		features, targets = features[idx], targets[idx]
+		return np.array([features[0]]), np.array([targets[0]])
 
 	def train(self, features, targets, epochs, lr):
-		Total_error = []
+		Error_rms = []
 		for e in range(epochs):
-			error_rms = 0
-			dw12, dw23, dw34 = np.zeros(self.w12.shape), np.zeros(self.w23.shape), np.zeros(self.w34.shape)
-			for x, y in zip(features, targets):
 
-				h0 = x
-				h1 = self.activate(np.dot(h0, self.w12))
-				h2 = self.activate(np.dot(h1, self.w23))
-				z  = self.activate(np.dot(h2, self.w34))
+			v0, o = self.shuffle_data(features, targets)
 
-				error = z - y
+			v1 = np.dot(v0,  self.w12)
+			y1 = self.activate(v1)
 
-				delError     = error*self.activatePrime(z)
+			v2 = np.dot(y1,  self.w23)
+			y2 = self.zactivate(v2)
 
-				hidden23_error = np.dot(self.w34, delError)
-				delHidden23 = hidden23_error*self.activatePrime(h2)
+			error = y2 - o
+			error_rms = 0.5*np.mean(error**2)
 
-				hidden12_error = np.dot(self.w23, delHidden23)
-				delHidden12 = hidden12_error*self.activatePrime(h1)
+			gradk = error * self.zactivatePrime(y2)
+			gradj = np.dot(gradk, self.w23.T) * self.activatePrime(y1)
 
-				dw34 += delError*h2[:,None]
-				dw23 += delHidden23*h1[:,None]
-				dw12 += delHidden12*x[:,None]
+			self.w23 += (lr * -gradk * y1.T)
+			self.w12 += (lr * -gradj * v0.T)
+		
+			Error_rms.append(error_rms)
 
-				error_rms += np.mean(error**2)
-
-			self.w34 += lr*dw34/features.shape[0]
-			self.w23 += lr*dw23/features.shape[0]
-			self.w12 += lr*dw12/features.shape[0]
-
-			Total_error.append(error_rms)
-
-		return Total_error
-
+		return Error_rms
 
 # Data for Neural Network
 
-epochs = 1000
-lr = 0.4
+epochs = 100000
+lr = 0.1
 
 features = np.array([[1, 1], [1, 0], [0, 1], [0, 0]])
 targets  = np.array([[0], [1], [1], [0]])
 
 nn = MultiLayerPerceptron(features, targets)
+Error_rms = nn.train(features, targets, epochs, lr)
 
-Total_error = nn.train(features, targets, epochs, lr)
 print(nn.run(features))
 
-plt.plot(range(epochs), Total_error)
+plt.plot(range(epochs), Error_rms)
+plt.show()
